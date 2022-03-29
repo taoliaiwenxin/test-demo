6424da8e3b62b8718ac558791a4e6cdf90fe6a8e
litao <happycoder.zzyr@gmail.com>
2022-03-29 23:34:12 +0800

    edit files

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 console.log('tesst');
 console.log('this is a dev branch');
 console.log('edit dev code');
+console.log("0000000000000000000000")
 
 function test() {
     console.log('this is function')
